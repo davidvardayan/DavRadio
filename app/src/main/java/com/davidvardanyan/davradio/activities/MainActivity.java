0c26c33f1619785ee1f7430e1559b1a8f69c4a85
David <dav-ss@mail.ru>
2019-08-10 07:21:55 -0700

    Initial commit

---
+++ app/src/main/java/com/davidvardanyan/davradio/activities/MainActivity.java
@@ -0,0 +1,25 @@
+package com.davidvardanyan.davradio.activities;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.davidvardanyan.davradio.R;
+import com.davidvardanyan.davradio.fragments.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        MainFragment mainFragment =  (MainFragment) fm.findFragmentById(R.id.container_main);
+
+        if (mainFragment == null){
+            mainFragment = MainFragment.newInstance("blah","klah");
+        }
+    }
+}
+
